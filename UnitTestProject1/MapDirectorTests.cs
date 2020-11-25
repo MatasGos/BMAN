880838466e4f09264bb6f57ed4471e508a29fe80
Matas Goštautas <32739863+MatasGos@users.noreply.github.com>
2020-11-25 15:26:31 +0200

    Create MapDirectorTests.cs

---
+++ UnitTestProject1/MapDirectorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class MapDirectorTests
+    {
+        [TestMethod()]
+        public void MapDirectorTest()
+        {
+            MapDirector mapDirector = new MapDirector(new ConcreteMapBuilder());
+
+        }
+
+        [TestMethod()]
+        public void getMapTest()
+        {
+            MapDirector mapDirector = new MapDirector(new ConcreteMapBuilder());
+            Assert.IsNull(mapDirector.getMap().units[0, 0]);
+        }
+
+        [TestMethod()]
+        public void constructMapTest()
+        {
+            MapDirector mapDirector = new MapDirector(new ConcreteMapBuilder());
+            mapDirector.constructMap();
+            Assert.IsTrue(mapDirector.getMap().units[0, 0] is Wall);
+        }
+    }
+}
