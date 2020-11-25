ec2bd517b030f6734fd9298b74c2c2de2d086e74
Matas Goštautas <32739863+MatasGos@users.noreply.github.com>
2020-11-25 15:37:57 +0200

    Create TeleporterMapBuilderTests.cs

---
+++ UnitTestProject1/TeleporterMapBuilderTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class TeleporterMapBuilderTests
+    {
+        [TestMethod()]
+        public void TeleporterMapBuilderTest()
+        {
+            MapDirector mapDirector = new MapDirector(new TeleporterMapBuilder());
+            mapDirector.constructMap();
+
+            Assert.IsTrue(mapDirector.getMap().units[1, 4] is Teleporter);
+        }
+    }
+}
