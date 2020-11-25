b913513ee09d681208596c65f5b5785cc6f049ef
Matas Goštautas <32739863+MatasGos@users.noreply.github.com>
2020-11-25 14:55:02 +0200

    Create ExplosionRangeBoostAlgorithmTests.cs

---
+++ UnitTestProject1/ExplosionRangeBoostAlgorithmTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class ExplosionRangeBoostAlgorithmTests
+    {
+        [DataTestMethod]
+        [DataRow("palyer", "id")]
+        public void UseBoostTest(string username, string id)
+        {
+            Player player = new Player(id, username, 0);
+            BoostAlgorithm boost = new ExplosionRangeBoostAlgorithm();
+            int oldPower = player.explosionPower;
+            boost.UseBoost(player);
+            Assert.AreEqual(oldPower + 1, player.explosionPower);
+        }
+    }
+}
