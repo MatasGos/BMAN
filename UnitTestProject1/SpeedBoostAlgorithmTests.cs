da9b0b840d193d2d2c3e256f4d8389d1fe1e3151
Matas Goštautas <32739863+MatasGos@users.noreply.github.com>
2020-11-25 14:55:04 +0200

    Create SpeedBoostAlgorithmTests.cs

---
+++ UnitTestProject1/SpeedBoostAlgorithmTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class SpeedBoostAlgorithmTests
+    {
+        [DataTestMethod]
+        [DataRow("palyer", "id")]
+        public void UseBoostTest(string username, string id)
+        {
+            Player player = new Player(id, username, 0);
+            BoostAlgorithm boost = new SpeedBoostAlgorithm();
+            int oldSpeed = player.speed;
+            boost.UseBoost(player);
+            Assert.AreEqual(oldSpeed + 1, player.speed);
+        }
+    }
+}
