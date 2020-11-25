c4d2772acfbc658a11c76524d177b7a77ac603ea
Matas Goštautas <32739863+MatasGos@users.noreply.github.com>
2020-11-25 15:33:53 +0200

    Create DefaultMapBuilderTests.cs

---
+++ UnitTestProject1/DefaultMapBuilderTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class DefaultMapBuilderTests
+    {
+        [TestMethod()]
+        public void DefaultMapBuilderTest()
+        {
+            MapDirector mapDirector = new MapDirector(new DefaultMapBuilder());
+            mapDirector.constructMap();
+
+            Assert.IsTrue(mapDirector.getMap().units[5, 1] is Box);
+        }
+    }
+}
