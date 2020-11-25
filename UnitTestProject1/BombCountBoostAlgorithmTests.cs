a4c1253db35be87ac81f021f727a6a0ab74e91f9
Matas Goštautas <32739863+MatasGos@users.noreply.github.com>
2020-11-25 15:26:29 +0200

    Create BombCountBoostAlgorithmTests.cs

---
+++ UnitTestProject1/BombCountBoostAlgorithmTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class BombCountBoostAlgorithmTests
+    {
+        [DataTestMethod]
+        [DataRow("palyer", "id")]
+        public void UseBoostTest(string username, string id)
+        {
+            Player player = new Player(id, username, 0);
+            BoostAlgorithm boost = new BombCountBoostAlgorithm();
+            int old = player.bombCount;
+            boost.UseBoost(player);
+            Assert.AreEqual(old + 1, player.bombCount);
+        }
+    }
+}
