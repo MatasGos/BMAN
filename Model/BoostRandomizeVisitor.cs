c356ad34c0043662b186e92ab285c8826702c19e
aksuram <47566165+aksuram@users.noreply.github.com>
2020-12-10 00:40:25 +0200

    fixes

--- Model/BoostRandomizeVisitor.cs
+++ Model/BoostRandomizeVisitor.cs
@@ -12,7 +12,7 @@ namespace Model
             if (block is Boost)
             {
                 Boost boost = (Boost)block;
-                int n = rand.Next(115);
+                int n = rand.Next(135);
                 if (n < 25)
                 {
                     boost.boostType = "speed";
@@ -33,17 +33,32 @@ namespace Model
                     boost.boostType = "explosion";
                     boost.algorithm = new ExplosionRangeBoostAlgorithm();
                 }
-                else if (n >= 100 && n < 105)
+                else if (n >= 100 && n < 110)
+                {
+                    boost.boostType = "mine";
+                    boost.algorithm = new MineCountBoostAlgorithm();
+                }
+                else if (n >= 110 && n < 115)
+                {
+                    boost.boostType = "supermine";
+                    boost.algorithm = new SuperMineCountBoostAlgorithm();
+                }
+                else if (n >= 115 && n < 120)
+                {
+                    boost.boostType = "superbomb";
+                    boost.algorithm = new SuperBombCountBoostAlgorithm();
+                }
+                else if (n >= 120 && n < 125)
                 {
                     boost.boostType = "teleporter";
                     boost.algorithm = new TeleporterChangeBoostAlgorithm();
                 }
-                else if (n >= 105 && n < 110)
+                else if (n >= 125 && n < 130)
                 {
                     boost.boostType = "boost";
                     boost.algorithm = new BoostRandomizeBoostAlgorithm();
                 }
-                else if (n >= 110 && n < 115)
+                else if (n >= 130 && n < 135)
                 {
                     boost.boostType = "armageddon";
                     boost.algorithm = new ArmageddonBoostAlgorithm();
